0ab7fe78d1919999f8ad6f40bf2ece572f84ffc8
mauriziosodano <maurizio.sodano@gmail.com>
2022-04-02 18:50:23 +0200

    added Customer operations

--- src/main/java/com/example/msscbrewery/services/CustomerServiceImpl.java
+++ src/main/java/com/example/msscbrewery/services/CustomerServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.msscbrewery.services;
 
+import com.example.msscbrewery.web.model.BeerDto;
 import com.example.msscbrewery.web.model.CustomerDto;
 import org.springframework.stereotype.Service;
 
@@ -13,5 +14,22 @@ public class CustomerServiceImpl implements CustomerService {
                 .id(UUID.randomUUID())
                 .name("Maurizio")
                 .build();
+    }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder().id(UUID.randomUUID())
+                .name(customerDto.getName())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+
+    }
+
+    @Override
+    public void deleteCustomer(UUID customerId) {
+
     }
 }
--- src/main/java/com/example/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/com/example/msscbrewery/web/controller/CustomerController.java
@@ -2,12 +2,11 @@ package com.example.msscbrewery.web.controller;
 
 import com.example.msscbrewery.services.CustomerService;
 import com.example.msscbrewery.web.model.CustomerDto;
+import com.example.msscbrewery.web.model.CustomerDto;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -17,12 +16,36 @@ public class CustomerController {
 
     private final CustomerService customerService;
 
-    public CustomerController(CustomerService beerService) {
-        this.customerService = beerService;
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
     }
 
     @GetMapping({"/{customerId}"})
-    public ResponseEntity<CustomerDto> getBeer(@PathVariable("customerId") UUID customerId) {
+    public ResponseEntity<CustomerDto> getCustomer(@PathVariable("customerId") UUID customerId) {
         return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity handlePost(@RequestBody CustomerDto customerDto) {
+
+        CustomerDto savedDto = customerService.saveNewCustomer(customerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        //TODO: add hostname in the headerValue
+        headers.add("Location", "/api/v1/beer/" + savedDto.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+    }
+
+    @PutMapping({"/{customerId}"})
+    public ResponseEntity handleUpdate(@PathVariable("customerId") UUID customerId, @RequestBody CustomerDto customerDto) {
+        customerService.updateCustomer(customerId, customerDto);
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping({"/{customerId}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void handleDelete(@PathVariable("customerId") UUID customerId) {
+        customerService.deleteCustomer(customerId);
     }
 }
